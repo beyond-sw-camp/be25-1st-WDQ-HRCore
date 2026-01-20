32f6e0861744761e9f9225e36a68494dac5da252
seonwoo <tjsdn8963@naver.com>
2026-01-20 20:36:55 +0900

    Update 시스템_인사관리_기능테스트.sql

--- sql/시스템_인사관리_기능테스트.sql
+++ sql/시스템_인사관리_기능테스트.sql
@@ -118,25 +118,11 @@ BEGIN
 END$$
 DELIMITER ;
 
-CALL register_employee_with_user(
-    'admin01.dev@company.com',
-    '인사담당자01',
-    '010-2345-5678',
-    '850814-1358901',
-    '신한',
-    '317-6688-415622',
-    '2021-02-05'
-);
-
-CALL register_employee_with_user(
-    'test02.dev@company.com',
-    '테스트사원02',
-    '010-1235-5778',
-    '950104-1353427',
-    '국민',
-    '106-2569-413677',
-    '2024-02-04'
-);
+CALL register_employee_with_user('admin01.dev@company.com','인사담당자01','010-2345-5678','850814-1358901','신한','317-6688-415622','2021-02-05');
+CALL register_employee_with_user('test01.dev@company.com','테스트사원01','010-1275-5378','960105-1653427','국민','127-2589-413677','2024-02-04');
+CALL register_employee_with_user('test02.dev@company.com','테스트사원02','010-1375-5778','950104-1653427','농협','137-2589-413677','2023-02-04');
+CALL register_employee_with_user('test03.dev@company.com','테스트사원03','010-1475-5778','940104-1653427','하나','157-2589-413677','2022-02-04');
+CALL register_employee_with_user('test04.dev@company.com','테스트사원04','010-1575-5778','930104-1653427','우리','167-2589-413677','2021-02-04');
 
 -- 확인
 SELECT * FROM employee;
